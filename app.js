ba9f086ff1874d26f65d354e5d35d7f14c534413
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2022-11-27 11:13:45 -0500

    Tile refresh and search bar complete

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const $tileContainer = $(".tile-container");
 const $infoContainer = $(".info-container");
 $infoContainer.hide();
 
-//============================== REQUEST TOKEN DATA FROM API ==============================//
+//============================= REQUEST TOKEN DATA FROM API =============================//
 const callAPI = () => {
   $.get("https://api.coincap.io/v2/assets", (response) => {
-    const tokenDataArray = response.data;
-    $tileContainer.children().remove();
+    tokenDataArray = response.data;
     for (let token of tokenDataArray) {
       createTokenTile(token);
     }
@@ -16,49 +15,62 @@ const callAPI = () => {
 };
 callAPI();
 
-//=========================== CREATE/APPEND TILE FOR EACH TOKEN ===========================//
+//========================== CREATE/APPEND TILE FOR EACH TOKEN ==========================//
 const createTokenTile = (token) => {
   const $tokenTile = $(`<div class="token-tile" id='${token.id}'></div>`);
-  const $tokenInfoBasic = $('<div class="token-basic"></div>');
+  const $tokenInfoBasic = $(`<div class="token-basic"></div>`);
   const $tokenPrice = $(`<div class="token-price">$${Number(token.priceUsd).toFixed(2)}</div>`);
   $tokenInfoBasic.html(`<b>${token.name}</b> (${token.symbol}): `);
   $tokenTile.append($tokenInfoBasic);
   $tokenTile.append($tokenPrice);
   $tileContainer.append($tokenTile);
 
-  // Clicking on tile displays expanded info in info bar
-  $tokenTile.click(() => {
-    const id = $tokenTile[0].id;
-    $.get(`https://api.coincap.io/v2/assets/${id}`, () => {
-      const $infoBar = $(`<div class="info-bar"></div>`);
-      const $infoBarHead = $(
-        `<div class="info-bar-head"><b>${token.name}</b> (${token.symbol})</div>`
-      );
-      const $infoBarBody = $(`<div class="info-bar-body">Price: $${token.priceUsd}</br>
+  const $infoBar = $(`<div class="info-bar" id="${token.id}-info"></div>`);
+  const $infoBarHead = $(`<div class="info-bar-head"><b>${token.name}</b> (${token.symbol})</div>`);
+  let $infoBarBody = $(`<div class="info-bar-body">Price: $${token.priceUsd}</br>
+            Rank: ${token.rank}</br>
+            Market Cap: $${Number(token.marketCapUsd).toFixed(2)}</br>
+            Supply: ${token.supply}</br>
+            Max Supply: ${token.maxSupply}</br>
+            Volume Last 24hr: ${token.volumeUsd24Hr}</br>
+            Percent Change 24hr: ${token.changePercent24Hr}
+            </div>`);
+  $infoBar.append($infoBarHead);
+  $infoBar.append($infoBarBody);
+  $infoBar.hide();
+  $infoContainer.append($infoBar);
+
+  // Clicking on tile displays expanded info bar
+  const infoBarDisplay = (event) => {
+    $infoContainer.children().hide();
+    const id = event.target.id + "-info";
+    $(`#${id}`).show();
+    $infoContainer.show();
+    const $close = $(`<button class="close">X</button>`);
+    $infoContainer.append($close);
+    $close.click(() => {
+      $infoContainer.hide();
+    });
+  };
+  $tokenTile.click(infoBarDisplay);
+};
+
+//================================= REFRESH TOKEN DATA =================================//
+const refreshPrice = () => {
+  $.get("https://api.coincap.io/v2/assets", (response) => {
+    tokenDataArray = response.data;
+    const $tokenPrice = $(".token-price");
+    let i = 0;
+    for (let token of tokenDataArray) {
+      $tokenPrice[i].innerText = `$${Number(token.priceUsd).toFixed(2)}`;
+      let $infoBarBody = $(".info-bar-body");
+      $infoBarBody[i].innerHTML = `Price: $${token.priceUsd}</br>
                 Rank: ${token.rank}</br>
                 Market Cap: $${Number(token.marketCapUsd).toFixed(2)}</br>
                 Supply: ${token.supply}</br>
                 Max Supply: ${token.maxSupply}</br>
                 Volume Last 24hr: ${token.volumeUsd24Hr}</br>
-                Percent Change 24hr: ${token.changePercent24Hr}
-                </div>`);
-      $infoContainer.children().remove();
-      $infoBar.append($infoBarHead);
-      $infoBar.append($infoBarBody);
-      $infoContainer.prepend($infoBar);
-      $infoContainer.show();
-    });
-  });
-};
-
-//===================================== REFRESH PRICE =====================================//
-const refreshPrice = () => {
-  $.get("https://api.coincap.io/v2/assets", (response) => {
-    const refreshedTokenDataArray = response.data;
-    const $tokenPrice = $(".token-price");
-    let i = 0;
-    for (let token of refreshedTokenDataArray) {
-      $tokenPrice[i].innerText = `$${Number(token.priceUsd).toFixed(2)}`;
+                Percent Change 24hr: ${token.changePercent24Hr}`;
       i++;
     }
   });
@@ -77,4 +89,7 @@ const searchBar = () => {
       token.style.display = "none";
     }
   }
+};
+const clear = () => {
+    $input.reset();
 };
